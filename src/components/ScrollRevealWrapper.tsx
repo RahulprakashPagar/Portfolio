ca4ad7b781a5a034228fcd50efecb8089c58f257
Rahul Pagar <88588401+RahulprakashPagar@users.noreply.github.com>
2026-05-29 22:55:27 +0100

    Delete src directory

--- src/components/ScrollRevealWrapper.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { useScrollReveal } from '@/hooks/useScrollReveal'
-
-export default function ScrollRevealWrapper({ children }: { children: React.ReactNode }) {
-  const ref = useScrollReveal(0.1)
-  return <div ref={ref}>{children}</div>
-}
